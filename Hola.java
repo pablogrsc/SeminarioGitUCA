f60f84c2cab6b98117df8ae7158f48a54f3b4674
pablogrsc <37184817+pablogrsc@users.noreply.github.com>
2018-05-15 18:50:54 +0200

    Modificado el fichero Hola.java

--- Hola.java
+++ Hola.java
@@ -4,5 +4,6 @@ public class Hola{
     
     public static void main(String[] args) {
         System.out.println("Java es una mierda");
+        System.out.println("Pero me obligan a estudiarlo");
     }
 }
